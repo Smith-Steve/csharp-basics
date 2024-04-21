2fbe4e1d97510698195762303cec31bac5c74735
smith-steve <Steve.A.Dore@outlook.com>
2024-04-20 18:27:21 -0700

    n

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int myInt = 5; int mySecondInt = 2;
+            Console.WriteLine(Math.Pow(2,2));
+
+
+        }
+    }
+}
