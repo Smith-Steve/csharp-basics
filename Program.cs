68294bfc716b54c55b76f0cd1648dce060220b37
smith-steve <Steve.A.Dore@outlook.com>
2024-04-20 21:02:47 -0700

    methods next

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Linq;
 
 namespace HelloWorld
@@ -8,10 +9,19 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            int myInt = 5; int mySecondInt = 2;
-            Console.WriteLine(Math.Pow(2,2));
+            int[] intsToCompress = new int[]{10, 15,20,25,30,35,40};
 
+            
+            int totalValue = intsToCompress[];
+            DateTime startTime = DateTime.Now;
+            //For loops.
+            int totalValue = 0;
 
+            foreach(int intForCompression in intsToCompress)
+             {
+                totalValue += intForCompression;
+             }
+             Console.WriteLine(totalValue);
         }
     }
 }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7810bc0312bf53d5cae2936b3221e415cbb0f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbe4e1d97510698195762303cec31bac5c74735")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
